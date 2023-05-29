8406403d038a53da04eb465feb4c3f359f2a2147
Praise Omogbemeh <onosithena@gmail.com>
2023-05-28 19:04:12 -0500

    Init commit

---
+++ src/test/java/codes/praise/submission/SubmissionApplicationTests.java
@@ -0,0 +1,13 @@
+package codes.praise.submission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubmissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
